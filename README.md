c3e905d509582f51ec36e805564aee83467080e5
Kulvinder Singh <kalvinsingh777@gmail.com>
2024-06-01 00:21:56 +0500

    Tilal_Sharqiyah_Trading is created

--- README.md
+++
@@ -1,4 +0,0 @@
-# Web_Development
-This is the code which I write throughout learning the Web Development.
-<br>
-Author- Kulvinder Singh
---
+++ script.js
@@ -0,0 +1,16 @@
+// document.addEventListener('DOMContentLoaded', () => {
+//     let slides = document.querySelectorAll('.slide');
+//     let currentSlide = 0;
+//     const intervalTime = 5000;
+//     let slideInterval;
+
+//     const nextSlide = () => {
+//         slides[currentSlide].classList.remove('current');
+//         currentSlide = (currentSlide + 1) % slides.length;
+//         slides[currentSlide].classList.add('current');
+//     };
+
+//     slideInterval = setInterval(nextSlide, intervalTime);
+
+
+// });
